95d761ca01a4a0fa6eb30da2d1d654eafc392733
M0rphy1 <jefemaestroasf@gmail.com>
2024-05-15 16:05:30 -0500

    modificar controlador

--- controlador/controladorT.php
+++ controlador/controladorT.php
@@ -1,21 +1,21 @@
 <?php
-$Vr= $_GET['var1'];
-if($Vr==1){
-    include("../vista/RegistroT.html");
-}else{
-    if($Vr==2){
+$Vr = isset($_GET['var1']) ? $_GET['var1'] : null;
+
+switch ($Vr) {
+    case 1:
+        include("../vista/RegistroT.html");
+        break;
+    case 2:
         include("../vista/ConsultarT.php");
-    
-    }else{
-        if($Vr==3){
-            include("../vista/ActualizarT.php");
-        
-        } else{
-            if($Vr==4){
-                include("../vista/EliminarT.html");
-            
-        }
-    }
-}
+        break;
+    case 3:
+        include("../vista/ActualizarT.php");
+        break;
+    case 4:
+        include("../vista/EliminarT.html");
+        break;
+    default:
+        echo "Opción no válida";
+        break;
 }
 ?>
