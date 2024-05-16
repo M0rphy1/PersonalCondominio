f5386b050a9ebdc1aa7d67774df1122b1ec850d0
gvallejo19 <137564901+gvallejo19@users.noreply.github.com>
2024-05-16 08:01:15 -0500

    Modelos
    Añado modelos

---
+++ modelo/buscarM.php
@@ -0,0 +1,35 @@
+<?php
+include('../config/conexion.php');
+
+// Verificar si se ha enviado una solicitud de búsqueda
+if(isset($_GET['buscar'])){
+    // Obtener el valor del parámetro 'codigo_limpieza' del formulario
+    $codigo_mantenimiento = $_GET['codigo_mantenimiento'];
+
+    // Realizar la consulta en la base de datos
+    $sql = "SELECT p.codigo AS codigo_personal, m.codigo AS codigo_mantenimiento, p.cedula, p.nombre, p.apellido, m.nombrearea, m.sueldo 
+            FROM personal p 
+            LEFT JOIN mantenimiento m ON p.codigo = m.codigopersonal 
+            WHERE m.codigo='$codigo_limpieza'";
+    $result = $conexion->query($sql);
+
+    // Verificar si se encontraron resultados
+    if ($result->num_rows > 0) {
+        // Redirigir a la vista de resultados con los datos obtenidos
+        echo '<script language="javascript">alert("Búsqueda exitosa");</script>';
+        include("../vista/ResultadosM.php");
+    } else {
+        // No se encontraron resultados, redirigir a una vista indicando la ausencia de resultados
+        echo '<script language="javascript">alert("Error, no hay ningún código perteneciente al área. Revise el área al que corresponde");</script>';
+        include("../vista/ConsultarM.php");
+        exit();
+    }
+} else {
+    // Si no se ha enviado una solicitud de búsqueda, redirigir a una página de error o a la página principal
+    include("../vista/RegistroM.html");
+    exit();
+}
+
+// Cerrar la conexión a la base de datos
+$conexion->close();
+?>
---
+++ modelo/guardarM.php
@@ -0,0 +1,21 @@
+<?php
+    include("../config/conexion.php");
+    $codigo_personal = $_POST['codigopersonal'];
+    $cedula = $_POST['cedula'];
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $nombrearea = $_POST['nombrearea'];
+    $sueldo = $_POST['sueldo'];
+
+    // Actualizar los datos en la tabla personal
+    $sql_personal = "UPDATE personal SET nombre = '$nombre', apellido= '$apellido' WHERE cedula='$cedula'";
+    mysqli_query($conexion, $sql_personal);
+
+    // Actualizar los datos en la tabla limpieza utilizando el código personal como clave foránea
+    $sql_limpieza = "UPDATE mantenimiento SET nombrearea = '$nombrearea', sueldo= '$sueldo' WHERE codigopersonal='$codigo_personal'";
+    mysqli_query($conexion, $sql_mantenimiento);
+
+    // Notificar al usuario que se ha registrado exitosamente
+    echo '<script language="javascript">alert("Se ha registrado exitosamente");</script>';
+    include("../vista/ConsultarM.php");
+?>
